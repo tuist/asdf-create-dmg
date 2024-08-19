a6f7a9dbd38c9185b57c427dba9b0f081e41f65c
fortmarek <marekfort@me.com>
2024-08-19 18:20:15 +0200

    Adapt for create-dmg

--- contributing.md
+++ contributing.md
@@ -5,7 +5,6 @@ Testing Locally:
 ```shell
 asdf plugin test <plugin-name> <plugin-url> [--asdf-tool-version <version>] [--asdf-plugin-gitref <git-ref>] [test-command*]
 
-# TODO: adapt this
 asdf plugin test create-dmg https://github.com/tuist/asdf-create-dmg.git "create-dmg --version"
 ```
 
--- lib/utils.bash
+++ lib/utils.bash
@@ -2,7 +2,6 @@
 
 set -euo pipefail
 
-# TODO: Ensure this is the correct GitHub homepage where releases can be downloaded for create-dmg.
 GH_REPO="https://github.com/create-dmg/create-dmg/"
 TOOL_NAME="create-dmg"
 TOOL_TEST="create-dmg --version"
@@ -31,8 +30,6 @@ list_github_tags() {
 }
 
 list_all_versions() {
-	# TODO: Adapt this. By default we simply list the tag names from GitHub releases.
-	# Change this function if create-dmg has other means of determining installable versions.
 	list_github_tags
 }
 
@@ -41,7 +38,6 @@ download_release() {
 	version="$1"
 	filename="$2"
 
-	# TODO: Adapt the release URL convention for create-dmg
 	url="$GH_REPO/archive/v${version}.tar.gz"
 
 	echo "* Downloading $TOOL_NAME release $version..."
@@ -61,7 +57,6 @@ install_version() {
 		mkdir -p "$install_path"
 		cp -r "$ASDF_DOWNLOAD_PATH"/* "$install_path"
 
-		# TODO: Assert create-dmg executable exists.
 		local tool_cmd
 		tool_cmd="$(echo "$TOOL_TEST" | cut -d' ' -f1)"
 		test -x "$install_path/$tool_cmd" || fail "Expected $install_path/$tool_cmd to be executable."
